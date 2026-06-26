601a87144f838d7c36f23407a3294e15ff8c5bd8
André Wittwer <andre.wittwer@nav.no>
2026-06-26 10:55:36 +0200

    test: geo regel

---
+++ src/test/kotlin/no/nav/flaggskipet/domene/vurdering/GeoTiltakspakkeRegelTest.kt
@@ -0,0 +1,29 @@
+package no.nav.flaggskipet.domene.vurdering
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import no.nav.flaggskipet.domain.vurdering.Adresse
+import no.nav.flaggskipet.domain.vurdering.Deltakelse
+import no.nav.flaggskipet.domain.vurdering.GeoTiltakspakkeRegel
+import no.nav.flaggskipet.domain.vurdering.VirksomhetUnderVurdering
+
+class GeoTiltakspakkeRegelTest :
+    FunSpec({
+        val adresseI50 = Adresse(type = "Foretningsadresse", postnummer = "7004", kommunenummer = "5001")
+        val adresseI30 = Adresse(type = "Foretningsadresse", postnummer = "0155", kommunenummer = "0301")
+
+        test("returns TILTAKSGRUPPE when fylke is in scope and sannsynglihet = 1.0") {
+            val regel = GeoTiltakspakkeRegel(fylkerIScopet = setOf("50"), sannsynlighet = 1.0)
+            regel.vurder((VirksomhetUnderVurdering(orgnummer = "123456789", adresse = adresseI50))) shouldBe Deltakelse.TILTAKSGRUPPE
+        }
+
+        test("returns KONTROLLGRUPPE when fylke is in scope and sannsynglihet = 0.0") {
+            val regel = GeoTiltakspakkeRegel(fylkerIScopet = setOf("50"), sannsynlighet = 0.0)
+            regel.vurder((VirksomhetUnderVurdering(orgnummer = "123456789", adresse = adresseI50))) shouldBe Deltakelse.KONTROLLGRUPPE
+        }
+
+        test("returns UTENFOR_SCOPE when fylke is out of scope") {
+            val regel = GeoTiltakspakkeRegel(fylkerIScopet = setOf("50"), sannsynlighet = 0.0)
+            regel.vurder((VirksomhetUnderVurdering(orgnummer = "123456789", adresse = adresseI30))) shouldBe Deltakelse.UTENFOR_SCOPE
+        }
+    })
