abcfa2f6ec3c0c4b379f036a9ce73c831f952f8f
André Wittwer <andre.wittwer@nav.no>
2026-07-07 13:06:51 +0200

    chore: remove unused code

--- src/main/kotlin/no/nav/flaggskipet/bootstrap/DependencyInjection.kt
+++ src/main/kotlin/no/nav/flaggskipet/bootstrap/DependencyInjection.kt
@@ -2,6 +2,8 @@ package no.nav.flaggskipet.bootstrap
 
 import io.ktor.server.application.Application
 import io.ktor.server.plugins.di.dependencies
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import no.nav.flaggskipet.domain.useCaseModule
 import no.nav.flaggskipet.infrastructure.clients.ereg.eregModule
 import no.nav.flaggskipet.infrastructure.database.config.databaseModule
@@ -10,7 +12,7 @@ internal fun Application.installDependencyInjection() {
     val config = environment.config
     dependencies {
         configModule(config)
-        metricsModule()
+        provide { PrometheusMeterRegistry(PrometheusConfig.DEFAULT) }
         databaseModule()
         eregModule()
         useCaseModule()
--- src/main/kotlin/no/nav/flaggskipet/domain/Module.kt
+++ src/main/kotlin/no/nav/flaggskipet/domain/Module.kt
@@ -1,7 +1,6 @@
 package no.nav.flaggskipet.domain
 
 import io.ktor.server.plugins.di.DependencyRegistry
-import io.ktor.server.plugins.di.provide
 import io.ktor.server.plugins.di.resolve
 import no.nav.flaggskipet.domain.vurdering.VurderTiltakspakkerUseCase
 
