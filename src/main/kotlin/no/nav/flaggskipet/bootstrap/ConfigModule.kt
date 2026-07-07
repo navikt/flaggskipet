13691975a9ace696c5cfe6a0518992d23823d813
André Wittwer <andre.wittwer@nav.no>
2026-07-07 08:00:12 +0200

    chore: remove unnecessary import

--- src/main/kotlin/no/nav/flaggskipet/bootstrap/ConfigModule.kt
+++ src/main/kotlin/no/nav/flaggskipet/bootstrap/ConfigModule.kt
@@ -2,7 +2,6 @@ package no.nav.flaggskipet.bootstrap
 
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.plugins.di.DependencyRegistry
-import io.ktor.server.plugins.di.provide
 import no.nav.flaggskipet.infrastructure.clients.ereg.toEregConfig
 import no.nav.flaggskipet.infrastructure.db.core.toDatabaseConfig
 
